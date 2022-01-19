99049449fec76e369cb8d5b792bbc0e5a80d2ec8
maesterzak <65318935+maesterzak@users.noreply.github.com>
2022-01-19 03:21:14 +0100

    more_styling1

--- pages/login.js
+++ pages/login.js
@@ -76,21 +76,21 @@ const LoginPage = () => {
           <div className="form-outline mb-4">
             <input onChange={onChange} defaultValue={username} name="username" type="text" id="form3Example3" className="form-control form-control-lg"
               placeholder="Enter a valid username address" />
-            <label className="form-label" for="form3Example3">Username</label>
+            <label className="form-label" htmlFor="form3Example3">Username</label>
           </div>
 
           
           <div className="form-outline mb-3">
             <input onChange={onChange} name='password' defaultValue={password} type="password" id="form3Example4" className="form-control form-control-lg"
               placeholder="Enter password" />
-            <label className="form-label" for="form3Example4">Password</label>
+            <label className="form-label" htmlFor="form3Example4">Password</label>
           </div>
 
           <div className="d-flex justify-content-between align-items-center">
             
             <div className="form-check mb-0">
               <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-              <label className="form-check-label" for="form2Example3">
+              <label className="form-check-label" htmlFor="form2Example3">
                 Remember me
               </label>
             </div>
@@ -111,7 +111,7 @@ const LoginPage = () => {
   <div className="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
     
     <div className="text-white mb-3 mb-md-0">
-      Copyright © 2020. All rights reserved.
+      Copyright ©devMasters 2021. All rights reserved.
     </div>
     
     <div>
