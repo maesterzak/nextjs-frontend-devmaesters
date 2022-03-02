3ef1f2717aff1764030f1c523fabbc5777d907e8
maesterzak <65318935+maesterzak@users.noreply.github.com>
2022-03-02 07:02:58 +0100

    added bootstrap.js

--- pages/Blog/index.js
+++ pages/Blog/index.js
@@ -155,7 +155,7 @@ const Home1 = ({ posts, threads, cate }) => {
         <title>SimpleLIFE | Blog Homepage</title>
         <meta name="keywords" content="Home" />
         <link rel="icon" href="/favicon1.ico" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossOrigin="anonymous"></script>
       </Head>
 
       <div>
